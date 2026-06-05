79cbd0be15e47066da95651118d0e17bb3d8d314
Damir <damirlyapin2001@gmail.com>
2026-06-05 19:55:26 +0300

    first step: init npm, add Makefile, add eslint

---
+++ Makefile
@@ -0,0 +1,14 @@
+install: 
+	npm ci
+
+lint:
+	npx eslint .
+
+vite:
+	npm run dev
+
+build:
+	npm run build
+
+preview:
+	npm run preview
---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  stylistic.configs.recommended,
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+])
